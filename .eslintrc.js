8756c45d21a88171b4eed2e99d872d0bae562422
ReAlign <realign@yeah.net>
2017-10-08 00:17:21 +0800

    re:travis & eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,10 +17,6 @@ module.exports = {
         "quotes": [
             "error",
             "single"
-        ],
-        "semi": [
-            "error",
-            "always"
         ]
     }
 };
